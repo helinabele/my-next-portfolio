af8a0835102c7bd227f959e957ed5889492f7437
helinabele <helinabmeaza@gmail.com>
2025-03-31 17:42:51 +0400

    analytics code added on layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "../styles/globals.css"; 
 import localFont from "next/font/local";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "../../public/fonts/Geist-Regular.ttf", // ✅ Use relative path correctly
@@ -24,6 +25,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+    <head>
+        {/* Google Analytics */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-81ZQGLR2NX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-81ZQGLR2NX');
+          `}
+        </Script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
